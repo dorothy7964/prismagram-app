4cc99a55e258dd129e7510b582af01bdd6d88acc
sample <dorothy_007@naver.com>
2019-11-12 14:27:44 +0900

    #14.0 Apollo Context

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,6 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 import Loader from "../../components/Loader";
+
+const FEED_QUERY = gql`
+    {
+        seeFeed {
+            id
+            location
+            caption
+            user {
+                id
+                userName
+                avatar
+            }
+            files {
+                id
+                url
+            }
+            isLiked
+            likeCount
+            comments {
+                id
+                text
+                user {
+                    id
+                    userName
+                }
+                createdAt
+            }
+            createdAt
+        }
+    }
+`;
 
 const View = styled.View`
     justify-content: center;
@@ -8,10 +41,9 @@ const View = styled.View`
     flex: 1;
 `;
 
-const Text = styled.Text``;
 
-export default () => (
-    <View>
-        <Loader />
-    </View>
-);
+export default () => {
+    const { data, loading } = useQuery(FEED_QUERY);
+    console.log("loading, data", loading, data);
+    return <View>{loading? <Loader /> : null}</View>;
+};
